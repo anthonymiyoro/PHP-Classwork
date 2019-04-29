7e2f1feb4ae7e53a470216d0c37296e38aa8dde0
Anthony Miyoro <amiyoro@gmail.com>
2019-04-29 15:37:00 +0300

    Copied solution

--- Assignment3/county_mix.php
+++ Assignment3/county_mix.php
@@ -2,76 +2,133 @@
 
 <!DOCTYPE html>
     <head>
-        <title>County Mix 🤢</title>
+        <title>🤢County Mix 🤢</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </head>
 
     <body>
-        <?php 
-            // a string of county data
-            $county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
-            2. Kwale. – Salim Mvurya – Jubilee,
-            3. Kilifi. – Amason Jeffah Kingi – ODM,
-            4. Tana River. – Dhadho Godhana – ODM,
-            5. Lamu. – Fahim Yasin Twaha – Jubilee,
-            6. Taita-Taveta. – Granton Samboja – WDM K,
-            7. Garissa. – Ali Bunow Korane – Jubilee,
-            8. Wajir. – mohamed Abdi Mohamud – Jubilee,
-            9. Mandera. – Ali Ibrahim Roba - Jubilee,
-            10. Marsabit – Mohamud Mohamed Ali – Jubilee,
-            11. Isiolo – Mohamed Abdi Kuti – Independent,
-            12. Meru – Kiraitu Murungi – Jubilee,
-            13. Tharaka Nithi – Onesmus Muthomi Njuki – Jubilee,
-            14. Embu – Martin Nyaga Wambora – Jubilee,
-            15. Kitui – Charitu Ngilu – Narc Kenya,
-            16. Machakos – Alfred Mutua – MCC,
-            17. Makueni – Kivutha Kibwana – WDM K.,
-            18. Nyandarua – Francis Kimemia – Jubilee,
-            19. Nyeri – Patrick Wahome Gakuru – Jubilee,
-            20. Kirinyaga. – Ann Waiguru – Jubilee,
-            21. Murang’a – Mwangi wa iria – Jubilee,
-            22. Kiambu. – Ferdinand Waititu Babayao – Jubilee,
-            23. Turkana. – Josphat Nanok – ODM,
-            24. West Pokot. – John Krop Lonyang’apuo. – KANU,
-            25. Samburu. – Moses Kasainie Lenolkulal – Jubilee,
-            26. Trans-Nzoia. – Patrick Khaemba – FORD Kenya,
-            27. Uasin gishu. – Jackson Mandago – Jubilee,
-            28. Elgeyo Marakwet. – Alex Tanui Tolgas – Jubilee,
-            29. Nandi. – Stephen Sang – Jubilee,
-            30. Baringo – Stanley K Kipris. – Jubilee,
-            31. Laikipia – Ndiritu Muriithi – Independent,
-            32. Nakuru. – Lee Kinyanjui – Jubilee,
-            33. Narok – Samuel Kuntai Ole Tunai – Jubilee,
-            34. Kajiado. – Joseph Ole Lenku – Jubilee,
-            35. Kericho. – Prof.Paul Chepkwony Kiprono – Jubilee,
-            36. Bomet. – Joyce Laboso – Jubilee,
-            37. Kakamega. – Wyclife Oparanya – ODM,
-            38. Vihiga. – Wilber Ottichilo – ODM,
-            39. Bungoma. – Wyclife Wafula Wangamiti – ODM,
-            40. Busia. – Sospeter Ojaamong – ODM,
-            41. Siaya. – Cornel Rasanga. – ODM,
-            42. Kisumu. – Peter Anyang Nyong’o. – ODM,
-            43. Homabay. – Cyprian Awiti – ODM,
-            44. Migori. – Zachary Okoth Obado – ODM,
-            45. Kisii. – James Ongware – ODM,
-            46. Nyamira. – John Nyangarama Obiena - ODM,
-            47. Nairobi. – Mike Sonko Mbuvi Kioko – Jubilee";
+    <?php 
+
+// a string of county data
+$county_data = "1. Mombasa. – Hassan Ali Joho – ODM,
+2. Kwale. – Salim Mvurya – Jubilee,
+3. Kilifi. – Amason Jeffah Kingi – ODM,
+4. Tana River. – Dhadho Godhana – ODM,
+5. Lamu. – Fahim Yasin Twaha – Jubilee,
+6. Taita-Taveta. – Granton Samboja – WDM K,
+7. Garissa. – Ali Bunow Korane – Jubilee,
+8. Wajir. – mohamed Abdi Mohamud – Jubilee,
+9. Mandera. – Ali Ibrahim Roba – Jubilee,
+10. Marsabit – Mohamud Mohamed Ali – Jubilee,
+11. Isiolo – Mohamed Abdi Kuti – Independent,
+12. Meru – Kiraitu Murungi – Jubilee,
+13. Tharaka Nithi – Onesmus Muthomi Njuki – Jubilee,
+14. Embu – Martin Nyaga Wambora – Jubilee,
+15. Kitui – Charitu Ngilu – Narc Kenya,
+16. Machakos – Alfred Mutua – MCC,
+17. Makueni – Kivutha Kibwana – WDM K.,
+18. Nyandarua – Francis Kimemia – Jubilee,
+19. Nyeri – Patrick Wahome Gakuru – Jubilee,
+20. Kirinyaga. – Ann Waiguru – Jubilee,
+21. Murang’a – Mwangi wa iria – Jubilee,
+22. Kiambu. – Ferdinand Waititu Babayao – Jubilee,
+23. Turkana. – Josphat Nanok – ODM,
+24. West Pokot. – John Krop Lonyang’apuo. – KANU,
+25. Samburu. – Moses Kasainie Lenolkulal – Jubilee,
+26. Trans-Nzoia. – Patrick Khaemba – FORD Kenya,
+27. Uasin gishu. – Jackson Mandago – Jubilee,
+28. Elgeyo Marakwet. – Alex Tanui Tolgas – Jubilee,
+29. Nandi. – Stephen Sang – Jubilee,
+30. Baringo – Stanley K Kipris. – Jubilee,
+31. Laikipia – Ndiritu Muriithi – Independent,
+32. Nakuru. – Lee Kinyanjui – Jubilee,
+33. Narok – Samuel Kuntai Ole Tunai – Jubilee,
+34. Kajiado. – Joseph Ole Lenku – Jubilee,
+35. Kericho. – Prof.Paul Chepkwony Kiprono – Jubilee,
+36. Bomet. – Joyce Laboso – Jubilee,
+37. Kakamega. – Wyclife Oparanya – ODM,
+38. Vihiga. – Wilber Ottichilo – ODM,
+39. Bungoma. – Wyclife Wafula Wangamiti – ODM,
+40. Busia. – Sospeter Ojaamong – ODM,
+41. Siaya. – Cornel Rasanga. – ODM,
+42. Kisumu. – Peter Anyang Nyong’o. – ODM,
+43. Homabay. – Cyprian Awiti – ODM,
+44. Migori. – Zachary Okoth Obado – ODM,
+45. Kisii. – James Ongware – ODM,
+46. Nyamira. – John Nyangarama Obiena – ODM,
+47. Nairobi. – Mike Sonko Mbuvi Kioko – Jubilee";
 
 
-            // You should not modify anything above this line
+// You should not modify anything above this line
 
-            // Your code starts here
+// Your code starts here
 
-            $Rows = 4; //Dynamic number for Rowss
-            $Cols = 3; // Dynamic number for Colsumns
-            echo '<table border="1">';
-            for($i=1;$i<=$Rows;$i++){ echo '<tr>';
-            for($j=1;$j<=$Cols;$j++){ echo '<td>' . mt_rand($i, $i*100) . mt_rand($j, $j*100) . '</td>'; }
-            echo '</tr>';
-            }
-            echo '</table>';
-        ?>
+$myArray = explode(',', $county_data);
+
+
+ echo "
+    <style>
+        table, th, td {
+        border: 1px solid black;
+        }
+    </style>
+    <table>
+    <tr>
+     <th>County Names</th>
+     <th>Governor Name</th>
+     <th>Political Party</th>
+    </tr>
+        ";
+
+for( $i = 0; $i<46; $i++ ) {
+
+ 
+
+    
+    $abc = $myArray[$i];
+    $abcd = explode('–', $abc);
+    // echo"$abc";
+    // echo"";
+
+
+    $website = $abcd[0];
+    $website = str_replace(' ', '', $website);
+    $website = explode('.', $website);
+    $website2 = $website[1];
+
+
+    echo "
+    <tr>
+    <td><a href='https://".$website2.".go.ke' target='_blank' >".$abcd[0]."</a></td>
+    ";
+
+       echo "
+
+     <td>".$abcd[1]."</td>
+
+
+     ";
+
+
+    
+    
+
+     echo "
+     <td>$abcd[2] </td>
+    </tr>
+  
+
+    
+    
+    ";
+
+}
+echo " </table>";
+
+
+?>
+
     </body>
 
     <footer>
