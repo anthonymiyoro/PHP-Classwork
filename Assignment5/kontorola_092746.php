baec84a4c6b3cc078cf7d08300c5c305672f0c67
Anthony Miyoro <amiyoro@gmail.com>
2019-06-01 15:59:21 +0300

    Renamed Assignment

---
+++ Assignment5/kontorola_092746.php
@@ -0,0 +1,25 @@
+<?php 
+
+class kontorola{
+public $users = [
+["id"=>1,"name"=>"Mwega Rafiki","email" => "mwega@example.com","admin"=>1,"gender"=>0],
+["id"=>2,"name"=>"Martha","email" => "martha@example.com","admin"=>0,"gender"=>1],
+["id"=>3,"name"=>"Mary","email" => "mary@example.com","admin"=>0,"gender"=>1],
+["id"=>4,"name"=>"Lazarus","email" => "laz@example.com","admin"=>0,"gender"=>0],
+["id"=>5,"name"=>"Lasaraleen","email" => "las@example.com","admin"=>0,"gender"=>1],
+["id"=>6,"name"=>"Shasta","email" => "shasta@example.com","admin"=>1,"gender"=>0],
+["id"=>7,"name"=>"Lucy","email" => "lucy@example.com","admin"=>1,"gender"=>1],
+["id"=>8,"name"=>"Tat Hermit","email" => "hermit@example.com","admin"=>0,"gender"=>0],
+["id"=>9,"name"=>"Bree","email" => "bree@example.com","admin"=>0,"gender"=>0],
+["id"=>10,"name"=>"Kin Loon","email" => "loon@example.com","admin"=>1,"gender"=>0],
+["id"=>11,"name"=>"Hwin","email" => "hwin@example.com","admin"=>0,"gender"=>1]
+];
+
+}
+
+class admin{
+
+}
+
+
+?>
